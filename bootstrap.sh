214cd574bf696bc03d9ab296fd4e726a4e4373d5
vagrant <vagrant@precise32.(none)>
2014-01-13 05:12:22 +0000

    add Vagrant setup

---
+++ bootstrap.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y python
+sudo apt-get install -y python-pip
+sudo apt-get install -y vim
+sudo apt-get install -y git-core python-django python-django-south python-simplejson
+sudo pip install -y Django==1.6.1
+sudo apt-get install -y chromium-browser
+
+sudo reboot
+echo 'Meow meow'
