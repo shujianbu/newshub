90d8ba148244c05a667f55c533e4696f25e8df89
Sophie Chou <sbchou@gmail.com>
2014-01-16 00:52:22 -0500

    add huanqiu parsing python code

---
+++ parsers/huanqiu/huanqiu.py
@@ -0,0 +1,30 @@
+
+import urllib2
+from bs4 import BeautifulSoup
+import re
+
+"""
+Script to fetch the contents of all the articles from huanqiu news site and save them by name.
+"""
+
+main_page = urllib2.urlopen("http://www.huanqiu.com/")
+
+main_soup = BeautifulSoup(main_page)
+
+#find all articles (denoted by date format)
+all_links = main_soup.findAll('a', href=re.compile('http://\w+\.huanqiu\.com/\S*\d\d\d\d-\d\d/\d+\.html'))
+
+for link in all_links:
+    #get each article
+    article = urllib2.urlopen(link.get('href').encode('utf-8'))
+    soup = BeautifulSoup(article)
+
+    outfile = file(soup.title.string.encode('utf-8'), "w")
+
+    text_divs = soup.findAll("div", { "class" : "text" })
+    for div in text_divs:
+        paragraphs = div.findAll("p")
+        for p in paragraphs:
+            outfile.write(p.string.encode('utf-8'))
+
+    outfile.close()
