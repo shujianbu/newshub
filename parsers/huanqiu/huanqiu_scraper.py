ad9ec99cd8c1d66e5582320d30e433137b1e4efb
vagrant <vagrant@precise32.(none)>
2014-01-19 00:07:21 +0000

    fix bugs in scraper, add test

--- parsers/huanqiu/huanqiu_scraper.py
+++ parsers/huanqiu/huanqiu_scraper.py
@@ -1,5 +1,5 @@
 
-import urllib2
+import requests
 from bs4 import BeautifulSoup as BS
 import re
 from time import strftime
@@ -11,36 +11,33 @@ scrapes http://www.huanqiu.com/ for article content and information.
 
 Extend functions for multiple sites.
 """
-def get_page(url):
-    try:
-        page = urllib2.urlopen(url, timeout=5)
-        
-    except urllib2.URLError as e:
-        print "REQUEST TIMED OUT"
-        return None  
 
-    return page
+def get_pages_with_retry(url):
+    """Try to get page with 5 retries"""
+    for i in range(5):
+        print "attempt", i+1
+        while True:
+            try:
+                r = requests.get(url)
+                return r
+            except requests.exceptions.Timeout:
+                print "Your request timed out."
+                continue
+            except requests.exceptions.TooManyRedirects:
+                print "Too Many Redirects. Check your URL. Stopping."
+            except requests.exceptions.RequestException as e:
+                print e
+            break
 
 
 def get_soup(url):
     """load html page given url"""
-    i = 0
-    again = True
-    
-    while (i < 5) and again:
-        page = get_page(url)
-        if page:
-            again = False
-        else:
-            i += 1
-    
-    if i == 5:
-        print "NO CONNECTION"
-        return 
-
-    soup = BS(page)
-    page.close() #memory leaks!
-    return soup
+    page = get_pages_with_retry(url)
+    if page:
+        soup = BS(page.content) 
+        return soup
+    else:
+        print "Failed to get page."
 
 def get_title(soup):
     """Return encoded title"""
@@ -64,46 +61,54 @@ def get_content(soup):
 
 def get_meta(soup):
     all_meta = soup.findAll('meta')
-    author = soup.find("meta", {"name": "author"}).get("content").encode("utf-8")
-    publishdate = soup.find("meta", {"name": "publishdate"}).get("content")
+    author_text = soup.find("meta", {"name": "author"})
+    publishdate_text = soup.find("meta", {"name": "publishdate"})
+
+    if author_text:
+        author = author_text.get("content").encode("utf-8")
+    else:
+        author = None
+    if publishdate_text:
+        publishdate = publishdate_text.get("content")
+    else:
+        publishdate = None
+
     return all_meta, author, publishdate
 
 def current_time():
     return strftime("%Y-%m-%d %H:%M:%S")
 
 
-def main():
+def test_all():
+    """Test scraper on all articles"""
     main = get_soup("http://www.huanqiu.com/")
     if main:    
         all_links = get_links(main)
+        num_links = len(all_links)
 
         i = 0
         for link in all_links:
             soup = get_soup(link.get('href').encode('utf-8'))
-            encoded_content = [line for line in get_content(soup)]
-            all_meta, author, publishdate = get_meta(soup)
-            parsedate = strftime("%Y-%m-%d %H:%M:%S")
+            if soup:
+                encoded_content = [line for line in get_content(soup)]
+                all_meta, author, publishdate = get_meta(soup)
+                parsedate = strftime("%Y-%m-%d %H:%M:%S")
 
-            #for testing
-            if encoded_content:
-                for p in encoded_content:
-                    print p
-            print all_meta
-            print author
-            print publishdate
-            print parsedate
+                #for testing
+                if encoded_content:
+                    for p in encoded_content:
+                        print p
+                print all_meta
+                print author
+                print publishdate
+                print parsedate
 
-            print "ARTICLE NUMBER", i
-            i += 1
+                print "ARTICLE NUMBER", i
+                i += 1
 
-            #TODO: Create class for above, format SQL
-
-
-
-
-
-
-
+    print "NUMBER OF SUCCESSFULLY FETCHED ARTICLES: ", i
+    print "TOTAL NUMBER OF ARTICLES", num_links
+    #TODO: Create class for above, format SQL
 
 
 
