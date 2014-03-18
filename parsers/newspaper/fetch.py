36d714334bdaba72ffac3ba9414987a6988e2711
GambleGear <zhushiyao.zsy@gmail.com>
2014-03-18 15:28:00 -0400

    some logical changes

--- parsers/newspaper/fetch.py
+++ parsers/newspaper/fetch.py
@@ -6,20 +6,10 @@ import MySQLdb
 
 from time import gmtime, strftime
 from newspaper import Config
-from xml.dom.minidom import parse
+#from xml.dom.minidom import parse
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
-
-def get_table():
-	db_identify = "mysql -X --default-character-set=utf8 -h newshub.c5dehgoxr0wn.us-west-2.rds.amazonaws.com"
-	db_authenticate = " -u newshub -pcolumbiacuj"
-	db_query = "\"use Newshub; select * from sampleTable order by url, Time_Check\""
-	db_connect = db_identify + db_authenticate + " -e " + db_query + " > DB_initial.xml"
-
-	# the DB file is ordered by url and Time_Check.
-	os.system(db_connect)
-	print "DB_initial is downloaded"
 
 def upload_meta(url, title, domain, text):
 	gTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
@@ -46,14 +36,12 @@ def get_meta(article, domain):
 	title = article.title
 	text = article.text
 
-	# need to detect whether the article is in the table or not
+	# Strictly spearking, we need to detect whether the article is in the DB or not
+	# but the overhead of time would be extremely huge.
 
 	upload_meta(url, title, domain, text)
 
 def get_articles():
-	# get the initial table before detecting new papers
-	get_table()
-
 	# get Chinese articles
 	i = 0
 	for url in open("list_ch.txt", 'r'):
