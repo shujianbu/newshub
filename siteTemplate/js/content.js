637329c3f25ef87048f9aa8d9d1aba1a151a4b9f
Shujian <shujianbu@Shujians-MacBook-Air.local>
2014-04-03 18:48:20 -0700

    delete the original fund

--- siteTemplate/js/content.js
+++ siteTemplate/js/content.js
@@ -2,15 +2,18 @@
 var cnDescr = "list_cn.json";
 console.log(cnDescr)
 
-$.getJSON(cnDescr, {
-  format: "json"
-}).done(
-	function( data ) {
-		console.log(data)
-    	$.each( data.Name, function(i, Name) {
-    		console.log(Name);
-      		$("<li>").append($("<a>").append(Name))
-      		.appendTo("#sidebar");
-    		});
-  	});
+
+
+
+// $.getJSON(cnDescr, {
+//   format: "json"
+// }).done(
+// 	function( data ) {
+// 		console.log(data)
+//     	$.each( data.Name, function(i, Name) {
+//     		console.log(Name);
+//       		$("<li>").append($("<a>").append(Name))
+//       		.appendTo("#sidebar");
+//     		});
+//   	});
 
