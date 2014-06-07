a02c6a42e807c24fdbd6a8c6c3389320b6634dcb
GambleGear <zhushiyao.zsy@gmail.com>
2014-06-07 14:53:21 -0400

    redirect of output file for exe*

---
+++ parsers/newspaper/exe_cleanDB.py
@@ -0,0 +1,23 @@
+import time
+import os
+
+# get the date for the first day of the month
+current_month = time.strftime('%Y-%m',time.localtime(time.time()))
+first_date = current_month + "-01"
+
+# get the month of the year
+current_month_only = time.strftime('%m',time.localtime(time.time()))
+
+db_identify = "mysql -X --default-character-set=utf8 -h newshub.c5dehgoxr0wn.us-west-2.rds.amazonaws.com"
+db_authenticate = " -u newshub -pcolumbiacuj -e \"use Newshub; "
+
+# build the query for database table articles
+db_query_selected = "delete from articles where Time_Publish<\"" + first_date + "\";"
+db_connect = db_identify + db_authenticate + db_query_selected
+os.system(db_connect)
+
+# build the query for database table deletions
+if(current_month_only == 01 or current_month_only == 07):
+	db_query_selected = "delete from deletions where Time_Publish<\"" + first_date + "\";"
+	db_connect = db_identify + db_authenticate + db_query_selected
+	os.system(db_connect)
